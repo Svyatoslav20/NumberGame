32eb96b33b8aa0010120ee143f7bad0d3f11908c
Svyatoslav20 <m68405990@gmail.com>
2025-12-18 20:38:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Загадали число от 1 до 10. Попробуй угадать его!");
+let secretNum = Math.floor(Math.random() * 10) +1;
+// console.log(secretNum)
+let attempt = 3;
+while(attempt > 0) {
+    let userNum = Number(prompt("Введите код"));
+    if(userNum < secretNum) {
+        console.log("Загаданное число больше..")
+        attempt = attempt - 1;
+        console.log("Осталось еще чуть чуть! Всего-лишь " + attempt + " попытки! У вас получиться")
+    }
+    else if(userNum === secretNum) {
+        console.log("Вы угадали! Сэкономили целых " + attempt + " попытки...");
+        break;
+    }
+    else {
+        console.log("Загаданное число меньше..")
+        attempt = attempt - 1;
+        console.log("Осталось еще чуть чуть! Всего-лишь " + attempt + " попытки! У вас получиться")
+    }
+}
+if(attempt === 0) {
+    console.log("Увы, но вы проиграли...")
+} 
+if(userNum > 10) {
+    console.log("Возможно вы ошиблись, однако нужно вписать число ОТ 1 до 10!")
+}
